0254c111aa4c738dddf54254fd59493f242b28bb
longjin <longjin@RinGoTek.cn>
2023-08-04 01:52:03 +0800

    使得部署的页面显示正常

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  base: '/rust_camp_tutorial/',
+  title: 'Rust训练营教程文档',
+  description: 'DragonOS-Rust camp',
+  themeConfig: {
+    nav: [
+		{
+			text: '首页', link: '/'
+		},
+	],
+
+  }
+}
